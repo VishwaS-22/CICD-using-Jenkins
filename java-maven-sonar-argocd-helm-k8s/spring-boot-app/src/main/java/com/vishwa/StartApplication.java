a6b7d6ce6c63a66b23ffb0cc166e96f2a5017f7d
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-10-31 19:11:08 +0530

    Update

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/vishwa/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/vishwa/StartApplication.java
@@ -1,4 +1,4 @@
-package com.abhishek;
+package com.vishwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
